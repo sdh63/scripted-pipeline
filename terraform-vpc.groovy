9dcc201b110654519ffe15596ad388999d1eaf70
sdh63 <selimhelvaci@gmail.com>
2020-09-29 01:53:20 +0000

    k

---
+++ terraform-vpc.groovy
@@ -0,0 +1,46 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: true, description: 'Do you want to run terraform apply', name: 'terraform_apply'),
+        booleanParam(defaultValue: false, description: 'Do you want to run terraform destroy', name: 'terraform_destroy')
+    ])
+])
+
+node{
+    stage("Pull Repo"){
+        cleanWs()
+        git branch: 'master', url: 'https://github.com/sdh63/terraform-vpc.git'
+    }
+
+    withEnv(['AWS_REGION=us-east-1']) {
+        withCredentials([usernamePassword(credentialsId: 'AWS_Jenkins_key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+            stage("Terraform Init"){
+                sh """
+                    bash setenv.sh dev.tfvars
+                    terraform init 
+                """
+            }
+
+            if(params.terraform_apply){
+                stage("Terraform Apply"){
+                    sh """
+                        terraform apply -var-file dev.tfvars -auto-approve
+                    """
+                }
+            }
+            else if(params.terraform_destroy){
+                stage("Terraform Destroy"){
+                    sh """
+                        terraform destroy -var-file dev.tfvars -auto-approve
+                    """
+                }
+            }
+            else {
+                stage("Terraform Plan"){
+                    sh """
+                        terraform plan -var-file dev.tfvars
+                    """
+                }
+            }           
+        }
+    }         
+}
