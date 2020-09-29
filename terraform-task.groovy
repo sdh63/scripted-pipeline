4258eb17e338d4bb005da90edd1bb06fb820292c
Selim Helvaci <68920154+sdh63@users.noreply.github.com>
2020-09-28 20:13:01 -0400

    Update terraform-task.groovy

---
+++ terraform-task.groovy
@@ -0,0 +1,20 @@
+node{
+    stage("Pull Repo"){
+        git branch: 'solution', url: 'https://github.com/ikambarov/terraform-task.git'
+    }
+
+    dir('sandbox/') {
+        withCredentials([usernamePassword(credentialsId: 'AWS_Jenkins_key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+            stage("Terraform Init"){
+                sh """
+                    terraform init 
+                """
+            }
+
+            stage("Terraform Apply"){
+                sh """
+                    terraform apply -auto-approve
+                """
+            }
+        }        
+    }
