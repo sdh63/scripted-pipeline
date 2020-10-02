64cb5a6dcce273bc72dca8746ee2676f94cd3ab2
sdh63 <selimhelvaci@gmail.com>
2020-10-02 20:35:30 +0000

    K

--- terraform-ec2.groovy
+++ terraform-ec2.groovy
@@ -44,7 +44,7 @@ def tfvar = """
     private_cidr1   = \"172.32.10.0/24\"
     private_cidr2   = \"172.32.11.0/24\"
     private_cidr3   = \"172.32.12.0/24\"
-    
+
     public_key    = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXUI8Mt0W/65CPA5rnR4auE8qVb08c6qR9Ca0yQaz9xM6EuShYX8jmktYbrdCIkZTMXbRF58CkWID/NHjYX4ZWZHwLi5uf2RfQegF67+kv6yJ2cgG4AsxUmWqlznxvm9615r8tpzBkKgsya58H+4aPRKqLJmhRm3ZZCa7t2HE7S+RR7fq+WtaQ3BMaKog9AVfHSEP8Gp4Ho7WUv5YlLXu5hlYC+m2oxrSCqXRFIhDtDuyphkzS93gDy8EVBkWnJFkoXT2LbVydcJaNCpEdjB1YFEEc1kMOXCAZ0w5N8PiWgdlY0lPeRXdH1RLX+WCM5FVOT9ujrq8PTQSYIkl2pek3"	
     ami_name        = "${params.ami_name}"
 """
@@ -57,7 +57,7 @@ node{
         writeFile file: "${params.environment}.tfvars", text: "${tfvar}"
     }
 
-    withCredentials([usernamePassword(credentialsId: 'aws_jenkins_key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+    withCredentials([usernamePassword(credentialsId: 'AWS_Jenkins_key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
         withEnv(["AWS_REGION=${aws_region_var}"]) {
             stage("Terrraform Init"){
                 sh """
