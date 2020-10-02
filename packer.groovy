05c4e2050e36e332756fd506df5812a427d8e307
sdh63 <selimhelvaci@gmail.com>
2020-10-02 21:54:59 +0000

    j

--- packer.groovy
+++ packer.groovy
@@ -23,24 +23,21 @@ node {
     stage('Pull Repo') {
         git 'https://github.com/ikambarov/packer.git'
     }
-    
+
     def ami_name = "apache-${UUID.randomUUID().toString()}"
 
-    withCredentials([usernamePassword(credentialsId: 'AWS_Jenkins_key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
-        withEnv(["AWS_REGION=${aws_region_var}", "PACKER_AMI_NAME=apache-${ami_name}"]) {
+    withCredentials([usernamePassword(credentialsId: 'aws_jenkins_key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        withEnv(["AWS_REGION=${aws_region_var}", "PACKER_AMI_NAME=${ami_name}"]) {
             stage('Packer Validate') {
                 sh 'packer validate apache.json'
             }
 
             stage('Packer Build') {
                 sh 'packer build apache.json'
-                
             }   
 
             stage('Build EC2 Instance') {
-                build wait: false, job: 'terraform-ec2', parameters: [booleanParam(name: 'terraform_apply', value:true, 
-                booleanParam(name:'terraform_destroy', value: false), string(name: 'environment', value: "${params.environment}"), 
-                string(name: 'ami_name', value: "${ami_name}")]
+                build wait: false, job: 'terraform-ec2', parameters: [booleanParam(name: 'terraform_apply', value: true), booleanParam(name: 'terraform_destroy', value: false), string(name: 'environment', value: "${params.environment}"), string(name: 'ami_name', value: "${ami_name}")]
             }
         }     
     }
